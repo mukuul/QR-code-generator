1f315d68ab11edba154f20b6c943d001ea9b8995
mukuul <mukul654@gmail.com>
2021-01-23 16:45:31 +0530

    check

--- src/screens/QR_Screen.js
+++ src/screens/QR_Screen.js
@@ -1,17 +1,17 @@
 import QRCode from 'react-native-qrcode-svg';
 import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
-const QR_Screen = ({ navigator, string}) => {
+const QR_Screen = ({ navigation, string }) => {
     return (
         <View>
             <QRCode
-            size={200}
-            bgColor='#000000'
-            fgColor='#FFFFFF'
-            value={string}  
-          />
+                size={200}
+                bgColor='#000000'
+                fgColor='#FFFFFF'
+                value={string}
+            />
         </View>
     )
 }
